066ebb1540b6e1636dd31c7188cfb2b236e967b0
Tom Black <tom.black06007@gmail.com>
2020-12-16 22:26:13 -0700

    worked on reverse string algorithm

--- reverseString.js
+++ reverseString.js
@@ -1,8 +1,18 @@
+// function reverseString(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     arr.join('');
+//     return arr;
+// }
+
 function reverseString(str) {
-    const arr = str.split('');
-    arr.reverse();
-    arr.join('');
-    return arr;
+    let reversed = '';
+
+    for (let character of str) {
+        reversed = character + reversed;
+
+        return reversed;
+    }
 }
 
 console.log(reverseString("Thomas"));
