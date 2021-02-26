aec47b56b566ed273630484109ea7acd4ca25369
Tom Black <tom.black06007@gmail.com>
2021-02-26 07:28:04 -0700

    Added a simpler way to complete the reverseInt algorithm.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -19,12 +19,23 @@
 
 
 //additional practice 
+// function reverseInt(n) {
+//    const reversed = n.toString().split('').reverse().join('');
+//    if(n < 0) {
+//        return parseInt(reversed) * -1;
+//    }
+//    return parseInt(reversed);
+// }
+
+//Additional practice
 function reverseInt(n) {
-   const reversed = n.toString().split('').reverse().join('');
-   if(n < 0) {
-       return parseInt(reversed) * -1;
-   }
-   return parseInt(reversed);
+    const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+    return parseInt(reversed) * Math.sign(n);
 }
 
 console.log(reverseInt(1234567890));
