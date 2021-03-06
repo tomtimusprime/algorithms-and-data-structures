d10060b2964faa0a4316960dead7e9dcf416501e
Tom Black <tom.black06007@gmail.com>
2021-03-06 13:03:23 -0700

    Added some practice to the pyramid algorithm.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -42,3 +42,54 @@ First create a function Pyramid
 Second find the midpoint - 
 
 */
+
+const pyramid1 = (n) => {
+    const midpoint = Math.floor((2 * n -1) / 2);
+
+    for(let row = 0; row < n; row++) {
+        let level = '';
+
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if(midpoint - row <= column && midpoint + row >= column) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+}
+
+const pyramid2 = (n) => {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let step = '';
+
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if(midpoint - row <= column && midpoint + row >= column) {
+                step += '#';
+            } else {
+                step += ' '
+            }
+        }
+        console.log(step);
+    }
+}
+
+const pyramid3 = (n) => {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+
+    for(let row = 0; row < n; row++) {
+        let step = '';
+
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if(midpoint - row <= column && midpoint + row >= column) {
+                step += '#';
+            } else {
+                step += ' ';
+            }
+        }
+        console.log(step);
+    }
+}
