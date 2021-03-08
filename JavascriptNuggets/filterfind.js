649705d198a0594661e989adc5714d390d475a2b
Tom Black <tom.black06007@gmail.com>
2021-03-08 16:53:12 -0700

    Added another file demonstrating filter and find methods.

---
+++ JavascriptNuggets/filterfind.js
@@ -0,0 +1,26 @@
+//Filter - returns a new array, can manipulate the size of the new array (unlike map()), returns based on condition.
+//Find - returns a single instance (object), returns first match, if not match - undefined.
+
+const people = [
+    { name: 'Bob', age: 20, position: 'developer'}, 
+    { name: 'Tom', age: 33, position: 'developer'}, 
+    { name: 'Ashley', age: 25, position: 'Professor'}, 
+    { name: 'Averi', age: 39, position: 'Photographer'}, 
+]
+
+const youngPeople = people.filter((person) => {
+    // if(person.age < 30) {
+    //     return person;
+    // }
+    return person.age < 30;
+});
+
+const tom = people.find((person) => {
+    return person.name === 'Tom';
+})
+
+const olderPerson = people.find((person) => person.age > 33);
+
+console.log(youngPeople);
+console.log(tom);
+console.log(olderPerson);
