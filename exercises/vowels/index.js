c295c3826dce15a80415e5aadf3b39af45d3471a
Tom Black <tom.black06007@gmail.com>
2021-03-14 11:01:59 -0700

    Added an alternative solution to finding vowels.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,18 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {
-    let numVals = 0;
-    //let checker = "aeiou";
-    const checker = ['a', 'e', 'i', 'o', 'u'];
+// function vowels(str) {
+//     let numVals = 0;
+//     //let checker = "aeiou";
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
 
-    for(char of str.toLowerCase()) {
-        if(checker.includes(char)) {
-            numVals++;
-        }
-    }
+//     for(char of str.toLowerCase()) {
+//         if(checker.includes(char)) {
+//             numVals++;
+//         }
+//     }
     
-    return numVals;
+//     return numVals;
+// }
+
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
