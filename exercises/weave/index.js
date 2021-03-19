bcf38ed586ff85a78be38b4abe0de3a53867c3fc
Tom Black <tom.black06007@gmail.com>
2021-03-18 21:39:50 -0700

    Added some additional practice to the weave algorithm.

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -42,4 +42,19 @@ function weave(sourceOne, sourceTwo) {
 
 }
 
-module.exports = weave;
+const weave1 = (sourceOne, sourceTwo) => {
+    const q = new Queue;
+
+    while(sourceOne.peek() || sourceTwo.peek()) {
+        if(sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+
+        if(sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+
+    return q;
+}
+module.exports = weave1;
