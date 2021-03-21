fccf9121b7923b4696cc803b49dec1feefc29275
Tom Black <tom.black06007@gmail.com>
2021-03-21 13:21:10 -0700

    Worked on the tree and node data structures.

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,29 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data; 
+        this.children = [];
+    }
+    add(data) {
+        this.children.push(new Node(data));
+    }
+    remove() {
 
-class Tree {}
+    }
+}
+
+class Tree {
+    constructor() {
+        let root = null;
+    }
+    traverseBF() {
+
+    }
+    traverseDF() {
+
+    }
+}
 
 module.exports = { Tree, Node };
