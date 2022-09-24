b5adf10e411326824c3f1ed23a1ae4b6f036a320
Tom Black <tom.black06007@gmail.com>
2022-09-23 23:12:18 -0600

    Added the c# translation of the reverseInt function.

--- reverseInt.js
+++ reverseInt.js
@@ -23,6 +23,12 @@ const reverseInt3 = (n) => {
     return n < 0 ? parseInt(reversed * -1) : parseInt(reversed);
 }
 
+//C# translation of reverseInt() function.
+// int reverseInt(int startingNumber){
+//     string reverse = string.Join("", Math.Abs(startingNumber).ToString().Reverse());
+//     return int.Parse(reverse) * Math.Sign(startingNumber);
+// }
+
 console.log(reverseInt(-5132));
 console.log(reverseInt(5156));
 console.log(reverseInt(189));
